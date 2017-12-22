8c3296f0ec70243902d81f294f20b222b50784c5
Jose Luis Oduber <jloduber@psytechdigital.local>
2017-12-22 14:49:08 +0000

    Added validation to the number of tweets per request according to twitter api docs

--- app/Http/Controllers/ShoutController.php
+++ app/Http/Controllers/ShoutController.php
@@ -17,14 +17,21 @@ class ShoutController extends Controller
 
     public function show(Request $request)
     {
+
+        $custom_validation_message = [
+            'number_of_last_tweets.between' =>
+                'Hi ' . $request->ip() . '! The :attribute must be between :min - :max. You can read more about it here - https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html',
+            'number_of_last_tweets.integer' => 'The :attribute value :input is not an integer.'
+        ];
+
         //Validate Request
         $validation = Validator::make($request->all(),[
             'username' => 'required',
-            'number_of_last_tweets' => 'required|integer|min:1',
-        ]);
+            'number_of_last_tweets' => 'required|integer|between:1,200',
+        ],$custom_validation_message);
 
         if($validation->fails()){
-            return $validation->errors()->toJson();
+            return $validation->errors();
         }
 
         return $this->repository->get_all_strings_to_shout($request->all());
